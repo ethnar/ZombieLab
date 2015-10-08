cc2c99a74b3e6c1336178fea509c42f9b74236b0
ethnar <ethnar86@gmail.com>
2015-10-08 22:12:19 +0200

    Tutorial - team selection tutorial

--- bin/app/js/services/tutorial.js
+++ bin/app/js/services/tutorial.js
@@ -4,45 +4,121 @@ angular.module('ZombieLabApp')
 
 .run(function ($interval, $rootScope, gameService, mapService) {
 	var tutorialHints = [{
+		/*********************************************************************************/
+		text: 'Select 4 characters for your squad<br/>Consider each character\'s items and skills',
 		condition: function () {
 			return $('.team-setup .item:visible').length;
 		},
-		text: 'Tap & hold to see item details',
+		considerDone: function () {
+			return $('.game').length;
+		},
 		getHighlights: function () {
-			return [
-				getElementHighlight($('.team-setup .item:visible'), 60)
-			];
+			return _.union(
+				getElementHighlight($('.team-setup .items').eq(1), 50),
+				getElementHighlight($('.skills').eq(1), 60)
+			);
+		}
+	}, {
+		/*********************************************************************************/
+		text: 'Tap & hold to see item details',
+		condition: function () {
+			return $('.team-setup .item:visible').length;
+		},
+		considerDone: function () {
+			return $('.game').length;
+		},
+		getHighlights: function () {
+			return getElementHighlight($('.team-setup .item-slot.small .item:visible,.team-setup .item:visible').eq(2), 60);
+		}
+	}, {
+		/*********************************************************************************/
+		text: 'Tap character image to include them in your team',
+		condition: function () {
+			var nothingSelected = $('.character-wrapper').length && !$('.character-wrapper.selected').length;
+			return delay('team-setup-any-character-selection', nothingSelected, 3);
+		},
+		considerDone: function () {
+			return $('.game').length || $('.character-wrapper.selected').length;
+		},
+		getHighlights: function () {
+			return getElementHighlight($('.character-panel .image'), 80);
+		}
+	}, {
+		/*********************************************************************************/
+		text: 'You will need 4 characters for your team',
+		condition: function () {
+			var charactersSelected = $('.character-wrapper.selected').length;
+			return delay('team-setup-4-characters-selection', charactersSelected < 4, 15);
+		},
+		considerDone: function () {
+			return $('.game').length || $('.character-wrapper.selected').length === 4;
+		},
+		getHighlights: function () {
+			return getElementHighlight($('.character-wrapper:not(.selected) .image'), 80);
+		}
+	}, {
+		/*********************************************************************************/
+		text: 'You can start the game',
+		condition: function () {
+			var charactersSelected = $('.character-wrapper.selected').length;
+			return delay('team-setup-go', charactersSelected == 4, 2);
+		},
+		considerDone: function () {
+			return $('.game').length;
+		},
+		getHighlights: function () {
+			return getElementHighlight($('button.start-game'), 50);
 		}
 	}];
 
-	function getElementHighlight(element, highlightSize) {
-		element = element.first();
-		var size = highlightSize || Math.max(element.outerHeight(), element.outerWidth());
-		var offset = size * Math.pow(2, 1/2) / 2;
-		return {
-			x: element.offset().left + element.outerWidth() / 2 - offset,
-			y: element.offset().top + element.outerHeight() / 2 - offset,
-			size: offset * 2
-		};
+	var delays = {};
+	function delay(category, condition, time) {
+		if (!condition || !delays[category]) {
+			delays[category] = new Date();
+		}
+		return (new Date() - delays[category]) / 1000 >= time;
+	}
+
+	function getElementHighlight(elements, highlightSize) {
+		var result = [];
+		elements.each(function () {
+			var element = $(this);
+			var size = highlightSize || Math.max(element.outerHeight(), element.outerWidth());
+			var offset = size * Math.pow(2, 1/2) / 2;
+			result.push({
+				x: element.offset().left + element.outerWidth() / 2 - offset,
+				y: element.offset().top + element.outerHeight() / 2 - offset,
+				size: offset * 2
+			});
+		});
+		return result;
 	}
 
 	function closeHint() {
-		$('.tutorial-highlight,.tutorial-text').remove();
-		gameService.unpause();
+		var tutorialElements = $('.tutorial-highlight,.tutorial-text');
+		tutorialElements.css('opacity', 0);
+		setTimeout(function () {
+			tutorialElements.remove();
+			gameService.unpause();
+		}, 300);
 	}
 
 	function displayHint(highlights, text) {
+		delays = {};
 		var last;
 		_.each(highlights, function (highlight) {
-			last = $('<div></div>')
+			var highlightElement;
+			last = highlightElement = $('<div></div>')
 				.addClass('tutorial-highlight')
 				.css('width', highlight.size)
 				.css('height', highlight.size)
 				.css('top', highlight.y)
 				.css('left', highlight.x)
-				.css('opacity', 0.6 / highlights.length)
 				.appendTo($('body'))
 				.click(closeHint);
+			setTimeout(function () {
+				highlightElement.css('opacity', 0.6 / highlights.length);
+			}, 100);
 		});
 		$('<div></div>')
 			.addClass('tutorial-text')
@@ -51,17 +127,24 @@ angular.module('ZombieLabApp')
 	};
 
 	setInterval(function () {
+		console.time('tutorial');
 		if (gameService.isPaused()) {
+			delays = {};
 			return;
 		}
 		_.each(tutorialHints, function (hint, idx) {
-			if (hint.condition()) {
-				gameService.pause();
-				displayHint(hint.getHighlights(), hint.text);
-				// remove hint
-				tutorialHints.splice(idx, 1);
-				$rootScope.$apply();
+			if (!hint.done) {
+				if (!gameService.isPaused() && hint.condition()) {
+					gameService.pause();
+					displayHint(hint.getHighlights(), hint.text);
+					// remove hint
+					hint.done = true;
+					$rootScope.$apply();
+				} else if (hint.considerDone && hint.considerDone()) {
+					hint.done = true;
+				}
 			};
 		});
+		console.timeEnd('tutorial');
 	}, 500);
 });
